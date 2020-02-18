2e2c6166c885b08c45ac8064a8fe64443d213c8e
daisuke.naka <ec2-user@ip-172-31-85-21.ec2.internal>
2020-02-18 14:25:41 +0000

    ver.1.7

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,4 +2,5 @@ class Store < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: true }
   has_many :stocks, dependent: :delete_all
   has_many :stock_details, dependent: :delete_all
+  has_many :users, dependent: :delete_all
 end
