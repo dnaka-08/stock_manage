40d1e39ee2385cc1664041946b821c851b91f792
daisuke.naka <ec2-user@ip-172-31-85-21.ec2.internal>
2020-03-15 00:02:27 +0000

    ver1.10 add mng_table

--- app/controllers/mng_tables_controller.rb
+++ app/controllers/mng_tables_controller.rb
@@ -4,10 +4,22 @@ class MngTablesController < ApplicationController
   before_action :set_user_session, only: [:index]
 
   def index
-    @stock_detail = StockDetail.joins(:operation, :store).where("stock_details.date >= '2020-03-01' and stock_details.date <= '2020-03-31'").group("stores.name").group("stock_details.date").group("operations.name").select("stores.name as store_name, stock_details.date as date, operations.name as operation_name, sum(stock_details.number) as stock_sum")
-    @stock = Stock.joins(:store).where("stocks.date >= '2020-03-01' and stocks.date <= '2020-03-31'").select("stores.name as store_name, stocks.date as date, stocks.total_number")
-    @stock_before = Stock.joins(:store).where("stocks.date < '2020-03-01'").group("stores.name").select("stores.name as store_name, sum(stocks.total_number) as total_number_sum")
-    @store = StockDetail.joins(:store).where("date >= '2020-03-01' and date <= '2020-03-31'").select("stores.name as store_name").distinct
+    if params[:target] == 'before' 
+      @date_begin = (Date.strptime(params[:date], '%Y-%m-%d') << 1).beginning_of_month.to_s
+      @date_end = (Date.strptime(params[:date], '%Y-%m-%d') << 1).end_of_month.to_s
+    elsif params[:target] == 'after' 
+      @date_begin = (Date.strptime(params[:date], '%Y-%m-%d') >> 1).beginning_of_month.to_s
+      @date_end = (Date.strptime(params[:date], '%Y-%m-%d') >> 1).end_of_month.to_s
+    else
+      @date_begin = Date.today.beginning_of_month.to_s
+      @date_end = Date.today.end_of_month.to_s
+    end
+    session[:date] = @date_begin
+    
+    @stock_detail = StockDetail.joins(:operation, :store).where("stock_details.date >= '#{@date_begin}' and stock_details.date <= '#{@date_end}'").group("stores.name").group("stock_details.date").group("operations.name").select("stores.name as store_name, stock_details.date as date, operations.name as operation_name, sum(stock_details.number) as stock_sum")
+    @stock = Stock.joins(:store).where("stocks.date >= '#{@date_begin}' and stocks.date <= '#{@date_end}'").select("stores.name as store_name, stocks.date as date, stocks.total_number")
+    @stock_before = Stock.joins(:store).where("stocks.date < '#{@date_begin}'").group("stores.name").select("stores.name as store_name, sum(stocks.total_number) as total_number_sum")
+    @store = StockDetail.joins(:store).where("date >= '#{@date_begin}' and date <= '#{@date_end}'").select("stores.name as store_name").distinct
     @operation = Operation.all
 
     @mng_table = []
@@ -16,7 +28,7 @@ class MngTablesController < ApplicationController
         @mng_rec = {}
         @mng_rec.store("store", store.store_name)
         @mng_rec.store("ope", ope.name)
-        (Date.parse('2020-03-01')..Date.parse('2020-03-31')).each do |date|
+        (Date.parse(@date_begin)..Date.parse(@date_end)).each do |date|
           @stock_detail.each do |stock_detail|
             if stock_detail.date == date and stock_detail.store_name == store.store_name and stock_detail.operation_name == ope.name
               @mng_rec.store(date, stock_detail.stock_sum)
@@ -39,7 +51,7 @@ class MngTablesController < ApplicationController
           break
         end
       end      
-      (Date.parse('2020-03-01')..Date.parse('2020-03-31')).each do |date|
+      (Date.parse(@date_begin)..Date.parse(@date_end)).each do |date|
         @stock.each do |stock|
           if stock.date == date and stock.store_name == store.store_name
             @mng_rec.store(date, total_number + stock.total_number)
