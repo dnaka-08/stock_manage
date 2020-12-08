ed864019a0082447321a2b05daf0ab3801c617d1
EC2 Default User <ec2-user@ip-172-31-8-216.ec2.internal>
2020-12-08 17:41:39 +0000

    ver1.33 fixed to be not display scroll

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -4,11 +4,10 @@ PDFKit.configure do |config|
     encoding:                "UTF-8",  # エンコーディング
     page_size:               "A3",     # ページのサイズ
     margin_top:              "0.25in", # 余白の設定
-    margin_right:            "0.5in",
+    margin_right:            "0.2in",
     margin_bottom:           "0.25in",
-    margin_left:             "0.5in",
+    margin_left:             "0.2in",
     orientation:             "Landscape",
-    lowquality:              true,  
-    disable_smart_shrinking: true
+    enable_smart_shrinking: true
   }
 end
